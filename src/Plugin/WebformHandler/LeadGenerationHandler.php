bd63678a505cbb009923059b4befbad87e5a75af
Jason Bugiada <jbugiada@gmail.com>
2023-01-03 16:11:08 -0500

    Excluded arrays in lead gen check

--- src/Plugin/WebformHandler/LeadGenerationHandler.php
+++ src/Plugin/WebformHandler/LeadGenerationHandler.php
@@ -7,17 +7,17 @@ use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
 
 /**
-* Form submission handler.
-*
-* @WebformHandler(
-*   id = "lead_generation_handler",
-*   label = @Translation("Lead Generation"),
-*   category = @Translation("settings"),
-*   description = @Translation("Converts lead generation Sales field type values from a no value token to an empty string or the replacement value in the type "),
-*   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
-*   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
-* )
-*/
+ * Form submission handler.
+ *
+ * @WebformHandler(
+ *   id = "lead_generation_handler",
+ *   label = @Translation("Lead Generation"),
+ *   category = @Translation("settings"),
+ *   description = @Translation("Converts lead generation Sales field type values from a no value token to an empty string or the replacement value in the type "),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ * )
+ */
 
 class LeadGenerationHandler extends WebformHandlerBase {
 
@@ -36,14 +36,17 @@ class LeadGenerationHandler extends WebformHandlerBase {
     $elements = $form->getElementsInitialized();
     // Loop through data and get key and values.
     foreach($data as $key => $val) {
-      // Check reg ex for a token.
-      if (preg_match('/^\[.*:.*\]$/', $val)) {
-        // Assign new value if the regex matched with the
-        // replacement_value field in the Sales field type
-        // or give it an empty string.
-        $new_val = (@$elements[$key]['#replacement_value']) ?: '';
-        // Assign the new value to the form.
-        $data[$key] = $new_val;
+      // If the value of $val is not an array
+      if (!is_array($val)) {
+        // Check reg ex for a token.
+        if (preg_match('/^\[.*:.*\]$/', $val)) {
+          // Assign new value if the regex matched with the
+          // replacement_value field in the Sales field type
+          // or give it an empty string.
+          $new_val = (@$elements[$key]['#replacement_value']) ?: '';
+          // Assign the new value to the form.
+          $data[$key] = $new_val;
+        }
       }
     }
     // Save the new data to the form.
